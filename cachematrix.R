235037aab40ab840baf9c52163f16357d372f24b
Sathyanarayanan Ragothaman <sathya003@outlook.com>
2015-08-24 00:49:59 +0530

    First commit
    First attempt to find the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The objective of this Programming assignment is to cache the Inverse of a matrix
 
-## Write a short comment describing this function
+#There are two functions in this assignment: makeCacheMatrix and cacheSolve
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#The function makeCacheMatrix obtains an input as matrix and caches its inverse
+makeCacheMatrix <- function(xmat = matrix()) {
+    mcache <- NULL
+    set <- function(ymat) {
+        xmat <<- ymat
+        mcache <<- NULL
+    }
+    get <- function() xmat
+    setinv <- function(solve) mcache <<- solve
+    getinv <- function() mcache
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
+#The function cacheSolve obtains an input as matrix and returns its inverse
 
-## Write a short comment describing this function
+#If it is cached, it returns the cached inverse, else computes the inverse 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(xmat, ...) {
+    ldet<-determinant(xmat)
+    if(ldet$modulus==-Inf) {
+        print("Inverse cannot be calculated")
+    }
+    mcache <- xmat$getinv()
+    if(!is.null(mcache)) {
+        message("getting cached data")
+        return(mcache)
+    }
+    matdata <- xmat$get()
+    mcache <- Solve(matdata, ...)
+    x$setinv(mcache)
+    mcache
 }
